66432b10fc28a4852e12bf71e14f4b2e27784666
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2021-07-20 20:02:21 +0200

    added new clion project with the neural network
    added new clion project with first version of the neural network implemented and to be tested

---
+++ LowPowerNeuralNetwork/cmake-build-debug/CMakeFiles/LowPowerNeuralNetwork.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LowPowerNeuralNetwork.dir/DeepNeuralNetwork.cpp.obj"
+  "CMakeFiles/LowPowerNeuralNetwork.dir/main.cpp.obj"
+  "LowPowerNeuralNetwork.exe"
+  "LowPowerNeuralNetwork.exe.manifest"
+  "LowPowerNeuralNetwork.pdb"
+  "libLowPowerNeuralNetwork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LowPowerNeuralNetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
