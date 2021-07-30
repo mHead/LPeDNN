fbe0a80d37902fb54e05f87cf3d82770a2bdd2e4
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2021-07-30 11:52:21 +0200

    SerialRead program added to main branch

---
+++ SerialRead/cmake-build-debug/CMakeFiles/SerialRead.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SerialRead.dir/SerialPort.cpp.obj"
+  "CMakeFiles/SerialRead.dir/main.cpp.obj"
+  "SerialRead.exe"
+  "SerialRead.exe.manifest"
+  "SerialRead.pdb"
+  "libSerialRead.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SerialRead.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
